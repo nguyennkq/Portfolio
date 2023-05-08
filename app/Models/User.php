c1ad7a3427fe6a1b20b534375ec759123f041c6e
nguyennkq <nguyenthega2k2@gmail.com>
2023-05-08 16:14:21 +0700

    must verify email

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+// Xác thực địa chỉ email bằng đường dẫn trong mailtrap
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -38,6 +39,7 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
+    // định dạng lại kiểu dữ liệu
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
