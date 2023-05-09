a6b17ac5588a8162666bbe8f30b3205f3f645622
nguyennkq <nguyenthega2k2@gmail.com>
2023-05-09 13:19:38 +0700

    add ui frontend into laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Home\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(HomeController::class)->group(function(){
+    Route::get("/",'HomeMain')->name('home');
 });
 
 Route::get('/dashboard', function () {
