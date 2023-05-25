1ff2c471cba123469aa127fbdb92f33785207457
nguyennkq <nguyenthega2k2@gmail.com>
2023-05-25 21:26:31 +0700

    show data resume into view

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Home;
 
 use App\Models\About;
 use App\Models\Skill;
+use App\Models\Resume;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -12,6 +13,7 @@ class HomeController extends Controller
     public function HomeMain(){
         $about_home = About::find(1);
         $skill_all = Skill::all();
-        return view('frontend.index', compact('about_home','skill_all'));
+        $resume_all= Resume::all();
+        return view('frontend.index', compact('about_home','skill_all','resume_all'));
     }
 }
